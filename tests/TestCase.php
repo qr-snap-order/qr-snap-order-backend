359d2d3534f29d5f6f054ec006e6b72a50e4d5d9
n-takeshita <naokisyuu@gmail.com>
2024-02-19 00:42:40 +0900

    add: use trait for test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Nuwave\Lighthouse\Testing\RefreshesSchemaCache;
+// use Nuwave\Lighthouse\Testing\TestsSubscriptions;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use MakesGraphQLRequests;
+    use RefreshesSchemaCache;
+    // use TestsSubscriptions;
 }
