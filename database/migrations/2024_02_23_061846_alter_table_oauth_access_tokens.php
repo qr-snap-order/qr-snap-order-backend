063d7f0f7e3e0a9d3bb9d9a47dcae30ade123da7
n-takeshita <naokisyuu@gmail.com>
2024-02-23 23:33:12 +0900

    update: oauth_access_tokens and oauth_clients user_id type to uuid

---
+++ database/migrations/2024_02_23_061846_alter_table_oauth_access_tokens.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('oauth_access_tokens', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+        Schema::table('oauth_access_tokens', function (Blueprint $table) {
+            $table->uuid('user_id')->index()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('oauth_access_tokens', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+        Schema::table('oauth_access_tokens', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->index();
+        });
+    }
+};
